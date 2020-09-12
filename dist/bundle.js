e78d269b91c58652a0c5b8bbbcc5e14880d74c2a
solomondove <solomondove@gmail.com>
2020-09-12 14:55:15 -0700

    added github link and fixed warping off screen

--- dist/bundle.js
+++ dist/bundle.js
@@ -882,6 +882,7 @@ function warpUtil(direction, ship) {
                 ship.oldPos = ship.pos;
                 holder[1] += -150;
                 if (holder[1] < 0) holder[1] = 0;
+                if (holder[1] > 500) holder[1] = 500;
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder);
             }
@@ -894,6 +895,7 @@ function warpUtil(direction, ship) {
                 ship.oldPos = ship.pos;
                 holder2[1] += 150;
                 if (holder2[1] > 500) holder2[1] = 500;
+                if (holder2[1] < 0) holder2[1] = 0;
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder2);
             }
@@ -915,7 +917,7 @@ function warpUtil(direction, ship) {
                 ship.teleport = true;
                 ship.oldPos = ship.pos;
                 holder4[0] += 200;
-                if (holder4[0] > 500) holder4[0] = 500;
+                if (holder4[0] > 750) holder4[0] = 750;
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder4);
             }
@@ -927,7 +929,8 @@ function warpUtil(direction, ship) {
                 ship.oldPos = ship.pos;
                 holder5[0] += -120;
                 holder5[1] += -120; 
-                if (holder5[0] > 500) holder5[0] = 500;
+                if (holder5[0] < 0) holder5[0] = 0; 
+                if (holder5[1] < 0) holder5[1] = 0; 
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder5);
             }
@@ -939,7 +942,8 @@ function warpUtil(direction, ship) {
                 ship.oldPos = ship.pos;
                 holder6[0] += 120;
                 holder6[1] += -120;
-                if (holder6[0] > 500) holder6[0] = 500;
+                if (holder6[0] > 750) holder6[0] = 750;
+                if (holder6[1] < 0) holder6[1] = 0;
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder6);
             }
@@ -951,7 +955,8 @@ function warpUtil(direction, ship) {
                 ship.oldPos = ship.pos;
                 holder7[0] += 120;
                 holder7[1] += 120;
-                if (holder7[0] > 500) holder7[0] = 500;
+                if (holder7[0] > 750) holder7[0] = 750;
+                if (holder7[1] > 500) holder7[1] = 500;
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder7);
             }
@@ -963,7 +968,8 @@ function warpUtil(direction, ship) {
                 ship.oldPos = ship.pos;
                 holder8[0] += -120;
                 holder8[1] += 120;
-                if (holder8[0] > 500) holder8[0] = 500;
+                if (holder8[0] < 0) holder8[0] = 0;
+                if (holder8[1] > 500) holder8[1] = 500;
                 ship.pos = [1000, 0];
                 ship.warpDelay(holder8);
             }
